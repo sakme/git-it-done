45f4306aef99332b4582cccc387de6075c0ec409
sakme <alan@akme.us>
2021-11-29 16:27:45 -0500

    linked issues to github

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 // fetch repo issues
 var getRepoIssues = function(repo) {
@@ -11,6 +12,11 @@ var getRepoIssues = function(repo) {
             if (response.ok) {
                 response.json().then(function(data) {
                     displayIssues(data);
+
+                    // check if api has paginated results
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
             } else {
                 alert("There was a problem with your request!");
@@ -63,4 +69,18 @@ var displayIssues = function(issues) {
     }   
 };
 
-getRepoIssues("facebook/react");
+// display warning
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit: ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("expressjs/express");
