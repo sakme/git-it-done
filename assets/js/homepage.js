34d046ef3dedb5190179a93aa8ac1fa891f6a0fd
sakme <alan@akme.us>
2021-11-30 07:05:01 -0500

    fix#3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 // fetch data from GitHub API
 var getUserRepos = function(user) {
@@ -90,5 +91,34 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+// search for featured repos
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            })
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+// button click handler
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
 // submit form event listener
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+// button click event listener
+languageButtonsEl.addEventListener("click", buttonClickHandler);
